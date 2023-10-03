468e04f7dcd916bc3dfe033e8ee30ecdd02251d9
Brijeshvekariya <bvekariya54@gmail.com>
2023-10-03 16:48:45 +0530

    updated

--- Python_assignment/Module-2(fundamentals of Python)/(2.11)n_sum.py
+++ Python_assignment/Module-2(fundamentals of Python)/(2.11)n_sum.py
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
+
 n = int(input("Enter last Number : "))
 sum = 0
 for i in range(1,n+1):
     sum+=i
-=======
-n = int(input("Enter last Number : "))
-sum = 0
-for i in range(1,n+1):
-    sum+=i
->>>>>>> 85cf8c3581af470733b0d951790fa5b4a6043b0f
+
 print("The Sum of %d Numbers is : "%n,sum)
