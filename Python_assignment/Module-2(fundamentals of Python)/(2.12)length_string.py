e499a69dc98d00609f5512ab2bc0f9ccc918e67c
Brijeshvekariya <bvekariya54@gmail.com>
2023-10-03 16:49:35 +0530

    updated

--- Python_assignment/Module-2(fundamentals of Python)/(2.12)length_string.py
+++ Python_assignment/Module-2(fundamentals of Python)/(2.12)length_string.py
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
+
 name = input("Enter Any String : ")
 count = 0
 for character in name:
     count+=1
 print("Length of given string is : ",count)
-=======
-name = input("Enter Any String : ")
-count = 0
-for character in name:
-    count+=1
-print("Length of given string is : ",count)
->>>>>>> 85cf8c3581af470733b0d951790fa5b4a6043b0f
 print(name[-1:])
