cf77129c3b87324e76ed077566ee19a886fd3da2
Brijeshvekariya <bvekariya54@gmail.com>
2023-10-03 16:46:33 +0530

    updated

--- Python_assignment/Module-2(fundamentals of Python)/(2.1)postive.py
+++ Python_assignment/Module-2(fundamentals of Python)/(2.1)postive.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 num = float(input("Enter any number : "))
 if num>0:
     print("\n")
@@ -10,16 +10,3 @@ else:
     print("\n")
     print(num,"is Negative Number")
     
-=======
-num = float(input("Enter any number : "))
-if num>0:
-    print("\n")
-    print(num,"is Positive Number")
-elif num ==0:
-    print("\n")
-    print(num,"is Zero")
-else:
-    print("\n")
-    print(num,"is Negative Number")
-    
->>>>>>> 85f4ca743050f340d8b66c8f1b35df8247cc10b4
