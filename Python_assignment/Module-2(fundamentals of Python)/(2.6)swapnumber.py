03758d33e51b8f0a35bd41da7b06449ac9261ebd
Brijeshvekariya <bvekariya54@gmail.com>
2023-10-03 16:47:06 +0530

    updated

--- Python_assignment/Module-2(fundamentals of Python)/(2.6)swapnumber.py
+++ Python_assignment/Module-2(fundamentals of Python)/(2.6)swapnumber.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 num1= int(input("Enter first Number : "))
 num2= int(input("Enter Second Number : "))
 num1 = num1+num2
@@ -14,20 +14,3 @@ num2 = c
 print("After Swapping two numbers : \nFirst Number : ",num1,"\nSecond Number : ",num2)
 
 
-=======
-num1= int(input("Enter first Number : "))
-num2= int(input("Enter Second Number : "))
-num1 = num1+num2
-num2 = num1 - num2
-num1 = num1 - num2
-print("---------------------------------\n")
-print("After Swapping two numbers : \nFirst Number : ",num1,"\nSecond Number : ",num2)
-
-print("\n\n----------------Using Three Variables-----------------\n")
-c = num1
-num1 = num2
-num2 = c
-print("After Swapping two numbers : \nFirst Number : ",num1,"\nSecond Number : ",num2)
-
-
->>>>>>> 85cf8c3581af470733b0d951790fa5b4a6043b0f
